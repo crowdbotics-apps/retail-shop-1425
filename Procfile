57e89f4f3ee37a7105f1126dfb0f356024bdf1ab
Crowdbotics <team@crowdbotics.com>
2019-03-17 07:39:43 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT retail_shop_1425.wsgi:application
---
+++ retail_shop_1425/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for retail_shop_1425 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "retail_shop_1425.settings")
+
+application = get_wsgi_application()
